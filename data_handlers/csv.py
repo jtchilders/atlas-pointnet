fixing normalization

data_handlers/csv.py
@@ -74,15 +74,18 @@ class BatchGenerator:
 
    def normalize_inputs(self,inputs):
       # shape: [B,N,4]
-      inputs[...,0] = (inputs[...,0] + self.eta_offset) / self.eta_diviser
-      inputs[...,1] = (inputs[...,1] + self.phi_offset) / self.phi_diviser
-      inputs[...,2] = (inputs[...,2] + self.r_offset) / self.r_diviser
-
-      Et = inputs[...,3]
-      Et_l2norm = np.linalg.norm(Et, axis=1, ord=2)
-      inputs[...,3] = Et / Et_l2norm[...,np.newaxis]
+      inputs[...,0] = norm_mean_std(inputs[...,0])
+      inputs[...,1] = norm_mean_std(inputs[...,1])
+      inputs[...,2] = norm_mean_std(inputs[...,2])
+      inputs[...,3] = norm_mean_std(inputs[...,3])
 
       return inputs
+
+
+def norm_mean_std(x):
+   mean = x.mean(axis=1).reshape(1,-1).transpose()
+   std = x.std(axis=1).reshape(1,-1).transpose()
+   return np.divide(x - mean,std,out=np.zeros_like(x,dtype='d'),where=std!=0)
 
 
 class CSVFileGenerator:
@@ -116,6 +119,3 @@ class CSVFileGenerator:
          return self.data['pid'][0]
       else:
          raise Exception('no data attribute')
-
-
-

pytorch/model.py
@@ -5,17 +5,18 @@ import CalcMean
 logger = logging.getLogger(__name__)
 torch.set_printoptions(sci_mode=False,precision=3)
 
+# detect device available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
 def get_model(config):
+
+   config['device'] = device
+   logger.info('device:             %s',device)
 
    if 'pointnet2d' in config['model']['model']:
       logger.info('using pointnet model')
       input_shape = config['data_handling']['image_shape']
-
-      # detect device available
-      device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-      config['device'] = device
-      logger.info('device:             %s',device)
 
       assert(len(input_shape) == 2)
 
@@ -25,7 +26,7 @@ def get_model(config):
       nClasses = len(config['data_handling']['classes'])
       logger.debug('nChannels = %s, nPoints = %s, nCoords = %s, nClasses = %s',nChannels,nPoints,nCoords,nClasses)
       model = pointnet.PointNet2d(nChannels,nPoints,nCoords,nClasses)
-
+      model.to(device)
       return model.float()
    elif 'pointnet1d' in config['model']['model']:
       logger.info('using pointnet model')
@@ -103,9 +104,9 @@ def train_model(net,opt,loss,acc,lrsched,trainds,validds,config,writer=None):
          logger.debug('got training batch %s',batch_counter)
          
          inputs = batch_data[0]
-         inputs.to(config['device'])
+         inputs.to(device)
          targets = batch_data[1]
-         targets.to(config['device'])
+         targets.to(device)
 
          logger.debug('inputs: %s targets: %s',inputs.shape,targets.shape)
 
@@ -116,7 +117,7 @@ def train_model(net,opt,loss,acc,lrsched,trainds,validds,config,writer=None):
          outputs,endpoints = net(inputs)
          logger.debug('got outputs: %s targets: %s',outputs,targets)
 
-         loss_value = loss(outputs,targets,endpoints,device=config['device'])
+         loss_value = loss(outputs,targets,endpoints,device=device)
          monitor_loss.add_value(loss_value)
          logger.debug('got loss')
 
@@ -179,7 +180,7 @@ def train_model(net,opt,loss,acc,lrsched,trainds,validds,config,writer=None):
                   outputs,endpoints = net(inputs)
                   #true_positive_accuracy,true_or_false_positive_accuracy,filled_grids_accuracy = accuracyCalc.eval_acc(outputs,targets,inputs)
 
-                  loss_value = loss(outputs,targets,endpoints,device=config['device'])
+                  loss_value = loss(outputs,targets,endpoints,device=device)
                   acc_value = acc(outputs,targets)
 
                   if writer: