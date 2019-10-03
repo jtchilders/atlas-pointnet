0055cec345664d4a5c4822103002bbb7eced3537
Taylor Childers <taylorchilders@gmail.com>
2019-10-03 08:42:35 -0500

    adding focal loss

--- pytorch/loss.py
+++ pytorch/loss.py
@@ -16,8 +16,8 @@ def get_loss(config):
    if 'func' not in loss_config:
       raise Exception('must include "func" loss section in config file')
 
-   if loss_config['func'] in locals():
-      return locals(loss_config['func'])
+   if loss_config['func'] in globals():
+      return globals()[loss_config['func']]
    elif 'CrossEntropyLoss' in loss_config['func']:
       weight = None
       if 'weight' in loss_config:
@@ -38,15 +38,15 @@ def get_loss(config):
       return torch.nn.CrossEntropyLoss(weight,size_average,ignore_index,reduce,reduction)
    
    else:
-      raise Exception('%s loss function is not recognized' % loss_config['func'])
+      raise Exception('%s loss function is not recognized; locals = %s' % (loss_config['func'],globals()))
 
 
 def get_accuracy(config):
    if 'CrossEntropyLoss' in config['loss']['func'] or 'pointnet_class_loss' in config['loss']['func']:
       
       return multiclass_acc
-   if config['loss']['func'] in ['pixel_wise_cross_entry','pixelwise_crossentropy_weighted']:
-      return pixel_wise_accuracy
+   if config['loss']['acc'] in globals():
+      return globals()[config['loss']['acc']]
    else:
       if 'func' not in config['model']:
          raise Exception('loss function not defined in config')
@@ -132,21 +132,35 @@ def pixel_wise_cross_entry(pred,targets,endpoints,device='cpu',reg_weight=0.001)
    return classify_loss
 
 
-def pixelwise_crossentropy_focal(pred,targets,endpoints,weights,device='cpu',gamma=2.,alpha=1.):
+def pixelwise_crossentropy_focalA(pred,targets,endpoints,weights,device='cpu',gamma=2.,alpha=1.):
 
    # pred.shape = [N_batch, N_class, N_points]
    # targets.shape = [N_batch,N_points]
+   # weights.shape = [N_batch,N_points]
 
    nclasses = pred.shape[1]
    npoints = targets.shape[1]
    nbatch = targets.shape[0]
 
-   model_out = torch.nn.LogSoftmax(dim=1)(pred)
-   ce = model_out * targets
+   # expand weights to [N_batch, N_class, N_points]
+   weights_onehot = weights.view(nbatch,1,npoints).repeat(1,nclasses,1)
+   logger.info('weights_onehot = %s',weights_onehot[0,...,0])
 
-   focal_weight = targets * torch.pow(1 - model_out,gamma)
+   targets_onehot = torch.LongTensor(nbatch,nclasses,npoints).zero_()
+   targets_onehot = targets_onehot.scatter_(1,targets.view(nbatch,1,npoints).long(),1).float()
+   logger.info('targets = %s',targets[0,0])
+   logger.info('targets_onehot = %s',targets_onehot[0,...,0])
 
-   focal_loss = focal_weight * alpha * ce * mask
+   model_out = -torch.nn.LogSoftmax(dim=1)(pred)  # [N_batch, N_class, N_points]
+   logger.info('model_out = %s',model_out[0,...,0])
+   ce = model_out * targets_onehot  # [N_batch, N_class, N_points]
+   logger.info('ce = %s',ce[0,...,0])
+
+   focal_weight = targets_onehot * torch.pow(1. - model_out,gamma)
+   logger.info('focal_weight = %s',focal_weight[0,...,0])
+
+   focal_loss = focal_weight * alpha * ce * weights_onehot
+   logger.info('focal_loss = %s',focal_loss[0,...,0])
 
    proportional_weights = []
    for i in range(len(class_ids)):
@@ -155,17 +169,39 @@ def pixelwise_crossentropy_focal(pred,targets,endpoints,weights,device='cpu',gam
    proportional_weights = proportional_weights.sum() / proportional_weights
    proportional_weights[proportional_weights == float('Inf')] = 0
 
-   # logger.info('weights = %s',weights)
+   proportional_weights = proportional_weights.view(nclasses,1).repeat(1,npoints)
+   proportional_weights = proportional_weights.view(1,nclasses,npoints).repeat(nbatch,1,1)
 
-   loss_value = torch.nn.CrossEntropyLoss(weight=proportional_weights,reduction='none')(pred,targets.long())
-   # logger.info('loss_value = %s',loss_value)
-   # logger.info('weights = %s',weights)
+   loss = focal_loss * proportional_weights
 
-   loss_value = loss_value * weights
-   # logger.info('loss_value = %s',loss_value)
-   loss_value = torch.mean(loss_value)
+   loss_value = torch.mean(loss)
 
    return loss_value
+
+
+def pixelwise_crossentropy_focal(pred,targets,endpoints,weights,device='cpu',gamma=2.,alpha=1.):
+   # from https://github.com/clcarwin/focal_loss_pytorch
+   # pred.shape = [N_batch, N_class, N_points]
+   # targets.shape = [N_batch,N_points]
+   # weights.shape = [N_batch,N_points]
+
+   nclasses = pred.shape[1]
+   # npoints = targets.shape[1]
+   # nbatch = targets.shape[0]
+
+   pred = pred.transpose(1,2)  # [N_batch,N_points,N_class]
+   pred = pred.contiguous().view(-1,nclasses)  # [N_batch*N_points,N_class]
+
+   targets = targets.view(-1,1).long()  # [N_batch*N_points,1]
+
+   logpt = torch.nn.functional.log_softmax(pred,dim=1)  # [N_batch*N_points,N_class]
+   logpt = logpt.gather(1,targets)  # [N_batch*N_points,1]
+   logpt = logpt.view(-1)
+   pt = torch.autograd.Variable(logpt.data.exp())
+
+   loss = -1 * (1-pt)**gamma * logpt
+
+   return loss.mean()
 
 
 def pixelwise_crossentropy_weighted(pred,targets,endpoints,weights=None,device='cpu'):
