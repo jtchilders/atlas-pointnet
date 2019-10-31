bfd66b09e86a9f55f4bfe60aad1e632458085a2e
Taylor Childers <taylorchilders@gmail.com>
2019-10-31 07:54:25 -0500

    adding custom target functions and choice of coords

--- data_handlers/pytorch_dataset_csv_semseg.py
+++ data_handlers/pytorch_dataset_csv_semseg.py
@@ -27,13 +27,30 @@ class CSVDataset(td.Dataset):
       #                      'Et': np.float32, 'pid': np.int32, 'true_pt': np.float32}
       self.col_dtype    = {'id': np.int64, 'index': np.int32, 'x': np.float32, 'y': np.float32,
                            'z': np.float32, 'eta': np.float32, 'phi': np.float32, 'r': np.float32,
-                           'Et': np.float32, 'pid': np.float32, 'n': np.float32, 
+                           'Et': np.float32, 'pid': np.float32, 'n': np.float32,
                            'trk_good': np.float32, 'trk_id': np.float32, 'trk_pt': np.float32}
 
       self.class_map = {}
       for i,entry in enumerate(self.class_ids):
          self.class_map[entry] = i
          self.class_map[-entry] = i
+
+      # set target function
+      self.get_target = getattr(self,config['data_handling']['target_func'])
+      self.nothing_class = self.class_ids[config['data_handling']['nothing_class_index']]
+
+      self.silicon_only = False
+      if 'silicon_only' in self.config['data_handling'] and self.config['data_handling']['silicon_only']:
+         self.silicon_only = True
+
+      self.coords = ['eta','phi','r','Et']
+      self.cartesian = False
+      if 'coords' in self.config['data_handling']:
+         if 'cartesian' in self.config['data_handling']['coords']:
+            self.coords = ['x','y','z','Et']
+            self.cartesian = True
+
+
 
    def __getitem__(self,index):
       filename = self.filelist[index]
@@ -47,46 +64,53 @@ class CSVDataset(td.Dataset):
       return self.get_input() + (self.get_target(),)
 
    def get_input(self):
-      if hasattr(self,'data'):
-         if 'silicon_only' in self.config['data_handling'] and self.config['data_handling']['silicon_only']:
-            input = self.data[self.data['id'] < 4e17]
-            input = input[['eta','phi','r','Et']]
-         else:
-            input = self.data[['eta','phi','r','Et']]
-
-         input = input.to_numpy()
-         input = np.float32(input)
-
-         # create a weight vector with 1's where points exist and 0's where they do not
-         weights = np.zeros(self.img_shape[0],dtype=np.float32)
-         weights[0:input.shape[0]] = np.ones(input.shape[0],dtype=np.float32)
-
-         padded_input = np.zeros(self.img_shape,dtype=np.float32)
-
-         padded_input[:input.shape[0],:] = input
-
-         input = padded_input.transpose()
-         
-         return input,weights
+      if self.silicon_only:
+         input = self.data[self.data['id'] < 4e17]
+         input = input[self.coords]
       else:
-         raise Exception('no data attribute')
+         input = self.data[self.coords]
 
-   def get_target(self):
-      if hasattr(self,'data'):
-         target = self.data['pid']
-         # init_length = len(target)
-         # logger.info('target[0:5] = %s',target.to_numpy()[0:5])
-         # logger.info('map = %s',self.class_map)
-         target = target.map(self.class_map)
-         target = np.int32(target.to_numpy())
-
-         padded_target = np.zeros(self.img_shape[0],dtype=np.int32)
-         padded_target[:target.shape[0]] = target
-
-         # logger.info('target[%s] = %s',init_length,target[init_length-5:init_length+5])
-         return torch.from_numpy(padded_target)
+      if not self.cartesian:
+         input['r'] = input['r'] / 1000.
       else:
-         raise Exception('no data attribute')
+         input[['x','y','z']] /= 1000.
+         input['Et'] /= 100.
+
+      input = input.to_numpy()
+      input = np.float32(input)
+
+      # create a weight vector with 1's where points exist and 0's where they do not
+      weights = np.zeros(self.img_shape[0],dtype=np.float32)
+      weights[0:input.shape[0]] = np.ones(input.shape[0],dtype=np.float32)
+
+      padded_input = np.zeros(self.img_shape,dtype=np.float32)
+
+      padded_input[:input.shape[0],:] = input
+
+      input = padded_input.transpose()
+      
+      return input,weights
+
+   def class_crossentropy_targets(self):
+      target = self.data['pid']
+
+      target = target.map(self.class_map)
+      target = np.int32(target.to_numpy())
+
+      padded_target = np.zeros(self.img_shape[0],dtype=np.int32)
+      padded_target[:target.shape[0]] = target
+
+      return torch.from_numpy(padded_target)
+
+   def bce_something_targets(self):
+      target = np.int32(self.data['pid'].to_numpy())
+
+      target = torch.from_numpy((target != self.nothing_class))
+
+      padded_target = torch.zeros(self.img_shape[0],dtype=torch.int)
+      padded_target[:target.shape[0]] = target
+
+      return padded_target
 
    def __len__(self):
       return self.len
